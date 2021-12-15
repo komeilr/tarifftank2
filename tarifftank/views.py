459476f8caa12999d32ae8a34c3226d6a81fb04a
Komeil <komeilr@gmail.com>
2021-12-15 15:10:16 -0500

    added skeleton views for search and 404

--- tarifftank/views.py
+++ tarifftank/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse, Http404
+from utils.classes import TariffCA
+
 
 def index(request):
     return render(request, 'index.html')
+
+def search(request):
+    query:str = request.GET.get('query')
+    
+    
+    # HS code search
+    if query.isdigit():
+        try:
+            t = TariffCA(query)
+            return JsonResponse(t.gen_tariff_dict())
+
+        except ValueError as e:
+            raise Http404
+            #return HttpResponseNotFound("Invalid HS Code. Please Try Again!")
+    
+    # Text Search    
+    elif query.isalpha():
+        return JsonResponse({"query": query})
+    
+    # multi text search
+    elif all(x.isalpha() or x.isspace() for x in query):
+        search_strings = query.split()
+        return JsonResponse({"query": search_strings})
+    
+    
+def hscodeNotFound(request, exception):
+    return render(request, '404.html', status=404)
